4ec4e53e7362070448fe6a5223326e9eb83f41f1
wanderson almeida <wandersonalmeida.eng@gmail.com>
2024-12-05 13:51:41 -0300

    implement userModel data

---
+++ src/main/java/br/com/wandersonalmeida/todolist/user/UserController.java
@@ -0,0 +1,23 @@
+package br.com.wandersonalmeida.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  /**
+   * String (texto)
+   * Integer (int) numeros inteiros
+   * Double (double) numeros 0.0000
+   * Float (float) Números 0.0000
+   * char (A C)
+   * Date (data)
+   * void (não tem retorno do metodo apenas uma logica sendo executada dentro do metodo)
+   */
+
+  public void create(){
+    
+  }
+  
+}
